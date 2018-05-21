0f4b089ceba2a36a349fcf29d4af9cf80465aa9e
FelixGehm <felixgehm@googlemail.com>
2018-05-21 16:03:47 +0200

    NavMeshAgent

---
+++ PointAndClickTechDemo/Assets/PlayerController.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour {
+    public Transform target;
+    public float zPosition = 0;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        transform.localPosition = new Vector3(target.localPosition.x, target.localPosition.z, zPosition);
+	}
+}
