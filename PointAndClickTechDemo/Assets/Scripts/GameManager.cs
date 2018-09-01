0a48e462d85a9d98cd9b67e9d31e3d4d062b5097
Florian Jann <florian.jann@hotmail.de>
2018-09-01 13:49:44 +0200

    Charakter & Anpassungen

--- PointAndClickTechDemo/Assets/Scripts/GameManager.cs
+++ PointAndClickTechDemo/Assets/Scripts/GameManager.cs
@@ -19,6 +19,8 @@ public class GameManager : MonoBehaviour
 
     [HideInInspector]
     public static bool graveVisited = false;
+
+
     // Use this for initialization
     void Start()
     {
--- PointAndClickTechDemo/Assets/Scripts/PlayerController.cs
+++ PointAndClickTechDemo/Assets/Scripts/PlayerController.cs
@@ -4,9 +4,10 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour {
     public Transform target;
-    public float zPosition = 0;
+    public float zOffset = 0;
     public float xOffset = 0;
-    public float yOffset = 0;
+    public float rotationOffset = 0;
+    
 
 	// Use this for initialization
 	void Start () {
@@ -15,6 +16,13 @@ public class PlayerController : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-        transform.localPosition = new Vector3(target.localPosition.x + xOffset, target.localPosition.z + yOffset, zPosition);
-	}
+        //transform.localPosition = new Vector3(target.localPosition.x + xOffset, target.localPosition.z + yOffset, zPosition);
+        transform.position = new Vector3(target.position.x + xOffset, transform.position.y, target.position.z + zOffset);
+
+        //transform.Rotate(Vector3.forward, angle);
+
+        float angle = 360.0f  - target.rotation.eulerAngles.y + rotationOffset;
+        transform.rotation = Quaternion.Euler(90, 0, angle);
+        
+    }
 }
